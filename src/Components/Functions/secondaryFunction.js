d6f04363bd2a338a3ec26f1627c6220612325222
Tetrapleg <rufmama@yandex.ru>
2020-09-01 12:48:43 +0300

    added fomatCurrensy in ModalItem.js

--- src/Components/Functions/secondaryFunction.js
+++ src/Components/Functions/secondaryFunction.js
@@ -1,5 +1,4 @@
 export const totalPriceItems = order => order.price * order.count;
 
-export function formatCurrency(props) {
-  return props.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB' })
-}
+export const formatCurrency = value => value.toLocaleString('ru-RU', 
+    { style: 'currency', currency: 'RUB' });
