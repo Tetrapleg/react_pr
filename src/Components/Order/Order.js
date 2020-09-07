c79ad5cd16855640ade7cc52acc3100a7532a0d4
Tetrapleg <rufmama@yandex.ru>
2020-09-07 13:55:08 +0300

    lesson12

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -47,7 +47,7 @@ const EmptyList = styled.p`
   text-align: center;
 `;
 
-export const Order = ({ orders, setOrders, setOpenItem }) => {
+export const Order = ({ orders, setOrders, setOpenItem, authentification, logIn, }) => {
 
   const deleteItem = index => {
     const newOrders = orders.filter((item, i) =>
@@ -83,7 +83,13 @@ export const Order = ({ orders, setOrders, setOpenItem }) => {
         <span>{totalCounter}</span>
         <TotalPrice>{formatCurrency(total)} </TotalPrice>
       </Total>
-      <ButtonCheckout>Оформить</ButtonCheckout>
+      <ButtonCheckout onClick={() => {
+        if (authentification) {
+          console.log(orders);
+        } else {
+          logIn()
+        }
+      }}>Оформить</ButtonCheckout>
     </OrderStyled>
   )
 }
